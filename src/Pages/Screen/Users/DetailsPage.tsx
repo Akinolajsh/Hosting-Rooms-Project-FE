d91a8d49eac5b4170ca64b1d7695326f9b457777
Akinolajsh <akinolajsh@gmail.com>
2023-08-26 13:06:59 +0100

    react icons install

--- src/Pages/Screen/Users/DetailsPage.tsx
+++ src/Pages/Screen/Users/DetailsPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 const DetailsPage = () => {
   return (
-    <div>DetailsPage</div>
+    <div className='w-full flex justify-center items-center bg-yellow'>
+      <div className='w-[90%] justify-center items-center flex flex-col '>
+        {/* Admin Details */}
+<div className='w-full flex flex-col  justify-center'>
+<div>
+Mermaid Suite - Toca da Capuava Granja Viana Cotia
+</div>
+</div>
+        {/* Admin Details */}
+      </div>
+    </div>
   )
 }
 
