2a12b8ba311cd8cff21cb3a8da878012b6cc8f25
Elizabeth Okorie <elizabethokorie407@gmail.com>
2023-08-27 07:36:35 +0100

    push

--- src/Pages/Auth/Register.tsx
+++ src/Pages/Auth/Register.tsx
@@ -1,12 +1,25 @@
-
+import pix from "../../assets/auth-bg-pix.jpg"
 
 const Register = () => {
   return (
-    <div className="w-full h-[100vh] bg-[url(`../../assets/auth-bg-pix.jpg`)] bg-cover bg-no-repeat bg-center flex">
+    <div className="w-full h-[100vh] flex bg-cover bg-center bg-no-repeat" 
+    style={{backgroundImage : `url(${pix})`}}
+    >
+      {/* cover */}
+      <div className="w-full h-[100vh] bg-green-950 bg-opacity-30 flex justify-center items-center">
         {/* card */}
-        <div className="w-[100%] h-[100%] flex flex-col justify-center items-center">
-            Register
+        <div className="w-[600px] h-[200px] bg-slate-600 flex">
+                {/* left */}
+                <div className="w-[50%] h-[100%] flex flex-col items-center border-green-100 border">
+                            left
+                </div>
+
+                {/* right */}
+                <div className="w-[50%] h-[100%] flex flex-col items-center  border-green-100 border">
+                            Right
+                </div>
         </div>
+      </div>
     </div>
   )
 }
