56dbd950e70b92716c40c2acc4b4fc686a955754
umeh emmanuel <eumeh3882@gmail.com>
2023-08-27 11:17:06 -0700

    emmanuel

--- src/EmmaProps/EmmaThird.tsx
+++ src/EmmaProps/EmmaThird.tsx
@@ -1,14 +1,217 @@
-
+import { TiTick } from "react-icons/ti";
+import { AiOutlineClose } from "react-icons/ai";
 
 const EmmaThird = () => {
   return (
-    <div className="w-full h-[800px] flex justify-center items-center">
-        <div className="w-[70%] h-full bg-red-600">
-            <div></div>
-
+    <div className="w-full h-[1600px] flex justify-center items-center">
+      <div className="w-[80%] h-full flex items-center flex-col">
+        {/*write up*/}
+        <div className="flex justify-center flex-col">
+          <h1 className="text-[40px] text-[#E41D57] text-center font-bold">
+            air<span className="text-[40px] text-black font-bold ">cover</span>
+          </h1>
+          <h3 className="text-center leading-[1px] text-[17px] font-medium">
+            for Hosts
+          </h3>
+          <h1 className="text-center text-[40px] font-semibold mt-[20px]">
+            Airbnb it with top‑to‑bottom protection
+          </h1>
         </div>
+        {/*down*/}
+        <div className="w-[80%] h-[50px] border-b-2 flex justify-between mt-[50px]">
+          <div></div>
+          <div className="flex justify-center items-center mb-[10px]">
+            <nav className="text-[20px] mr-[70px]">Airbnb</nav>
+            <nav className="text-[20px]">Competitors</nav>
+          </div>
+        </div>
+        {/*good and bad*/}
+        <div className="w-[80%] h-[170px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-bold text-[20px] pt-[20px]">
+              Guest identity verification
+            </div>
+            <div className="mt-[20px] pb-[30px] text-left font-light">
+              Our comprehensive verification system checks details such as name,{" "}
+              <br /> address, government ID and more to confirm the identity of
+              guests <br /> who book on Airbnb.
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-green-400">
+              <TiTick />
+            </nav>
+          </div>
+        </div>
+        {/*another*/}
+        <div className="w-[80%] h-[170px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-bold text-[20px] pt-[20px]">
+              Reservation screening
+            </div>
+            <div className="mt-[20px] pb-[30px] text-left font-light">
+              Our proprietary technology analyzes hundreds of factors in each{" "}
+              <br /> reservation and blocks certain bookings that show a high
+              risk for <br /> disruptive parties and property damage.
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*another1*/}
+        <div className="w-[80%] h-[150px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-bold text-[20px] pt-[20px]">
+              $3M damage protection
+            </div>
+            <div className="mt-[20px] pb-[30px] text-left font-light">
+              Airbnb reimburses you for damage caused by guests to your home{" "}
+              <br /> and belongings and includes these specialized protections:
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*the small ones*/}
+        <div className="w-[80%] h-[70px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-light text-[20px] pt-[20px]">
+              Art & valuables
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*small one2*/}
+        <div className="w-[80%] h-[70px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-light text-[20px] pt-[20px]">Auto & boat</div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*small one3*/}
+        <div className="w-[80%] h-[70px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-light text-[20px] pt-[20px]">Pet damage</div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*small one4*/}
+        <div className="w-[80%] h-[70px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-light text-[20px] pt-[20px]">Income loss</div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*small one5*/}
+        <div className="w-[80%] h-[70px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-light text-[20px] pt-[20px]">
+              Deep cleaning
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        {/*another 3*/}
+        <div className="w-[80%] h-[150px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-bold text-[20px] pt-[20px]">
+              $1M liability insurance
+            </div>
+            <div className="mt-[20px] pb-[30px] text-left font-light">
+              You’re protected in the rare event that a guest gets hurt or their{" "}
+              <br />
+              belongings are damaged or stolen.
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-green-400">
+              <TiTick />
+            </nav>
+          </div>
+        </div>
+        {/*another 4*/}
+        <div className="w-[80%] h-[150px] border-b-2 flex justify-between">
+          <div>
+            <div className="font-bold text-[20px] pt-[20px]">
+              24-hour safety line
+            </div>
+            <div className="mt-[20px] pb-[30px] text-left font-light">
+              If you ever feel unsafe, our app provides one-tap access to
+              specially- <br /> trained safety agents, day or night.
+            </div>
+          </div>
+          <div className="flex  pt-[20px]">
+            <nav className="text-[30px] mr-[140px] text-green-400">
+              <TiTick />
+            </nav>
+            <nav className="text-[30px] mr-[40px] text-[#E12C32]">
+              <AiOutlineClose />
+            </nav>
+          </div>
+        </div>
+        <div className="w-[80%] pt-[20px] ">
+          Comparison is based on public information and free offerings by top
+          competitors as of 10/22. Find details and exclusions{" "}
+          <span className="font-bold underline hover:cursor-pointer duration-300 "> here.</span>
+        </div>
+        <button className="w-[120px] h-[40px] flex justify-center items-center border rounded-md border-black
+        hover:cursor-pointer mt-[50px]">Learn more</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default EmmaThird
+export default EmmaThird;
