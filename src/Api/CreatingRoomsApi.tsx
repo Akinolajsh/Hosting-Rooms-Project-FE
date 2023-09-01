f7a94c0fc0450407e1c07e403d9bfe592312c1fe
jecinta2003 <onyemaobijecintaugochi@gmail.com>
2023-09-01 03:20:19 +0100

    creating room api jecinta

---
+++ src/Api/CreatingRoomsApi.tsx
@@ -0,0 +1,46 @@
+import axios from "axios"
+
+const url:string = "http://localhost:2659/api/v1"
+
+export const createRooms =async(data: any, userID: any)=>{
+    try {
+   const config:any = {
+        "content-type": "multipart/formdata"
+    }
+    return await axios.post(`${url}/${userID}/createRoom`, config,data).then((res:any)=>{
+        return res.data.data
+    })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const viewAllCreateRooms =async()=>{
+    try {
+        return await axios.get(`${url}/viewRooms`).then((res:any)=>{
+            return res.data.data
+        })
+        } catch (error) {
+            console.log(error)
+        }
+}
+export const viewOneCreateRooms =async(roomID:any)=>{
+    try {
+        return await axios.get(`${url}/${roomID}/viewRoom`).then((res:any)=>{
+            return res.data.data      
+        })
+        } catch (error) {
+            console.log(error)
+        }
+}
+
+export const viewUserRoom = async(userID: any)=>{
+    try {
+        return await axios.get(`${url}/viewUserRoom`,userID).then((res:any)=>{
+            return res.data.data
+        })
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
